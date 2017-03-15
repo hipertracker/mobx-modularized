2255aca3a2a8cde975cf3f6658547e41e60303a6
Jaroslaw Zabiello <hipertracker@gmail.com>
2017-03-15 15:13:27 +0000

    code formating

--- src/stores/users-management/sidebar/sidebar.mobx-store.js
+++ src/stores/users-management/sidebar/sidebar.mobx-store.js
@@ -1,9 +1,7 @@
 import { action, computed, observable} from 'mobx';
 import Child1 from './child1/child1.mobx-store';
 import Child2 from './child2/child2.mobx-store';
-
 import { injectStores } from '../../../decorators';
-
 
 @injectStores(Child1, Child2)
 export default class Sidebar {
