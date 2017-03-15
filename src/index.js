411b50e3377386188c25990a24754e1eb329d050
Jaroslaw Zabiello <hipertracker@gmail.com>
2017-03-15 15:10:34 +0000

    rename modules to stores

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import { Provider } from 'mobx-react';
 import { render } from 'react-dom';
 import UsersManagement from './components/users-management/users-management.react-component';
-import Store from './modules/users-management/users-management.mobx-store';
+import Store from './stores/users-management/users-management.mobx-store';
 
 export class App extends Component {
     constructor(props) {
